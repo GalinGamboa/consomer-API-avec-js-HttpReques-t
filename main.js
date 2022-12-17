0513ab3e5c1fd3e6678a502c9536473fd6898b06
GalinGamboa <galin_71@hotmail.com>
2022-12-17 17:33:03 +0100

    Consomer API avec js 01

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// buttons  Précedent et Prochain
 let pages = 1;
 
   document.querySelector('.btn_precedent').addEventListener('click', function(){
@@ -15,43 +16,46 @@ let pages = 1;
   })
 
 
-function chargerFilms () {
 
-  let films = new XMLHttpRequest
+  // Function Charger Films
 
-    films.addEventListener('load', ()=>{
-    let data = JSON.parse(films.responseText)
+function chargerFilms () {                                      // Créer la fonction chargerFilms
+  
+  //  HttpRequest 
+  
+  // étape 1
+  let films = new XMLHttpRequest                                //  Créer nouvelle requête Films
+  
+  // étape 2
+  films.addEventListener('load', ()=>{                          // Event Load a Films
+    let data = JSON.parse(films.responseText)                   //  data  ====> json
     //console.log(data.results);
-
     let peliculas = ""
-    data.results.forEach(pelicula => {
-      //console.log(pelicula.title)
-
-      peliculas += `
-          <div class="film">
-            <img  class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"/>
-            <h3 class = " title " >${pelicula.title}</h3>
-          </div>
-          
-        `
-
-    });
-
-    document.querySelector(".container").innerHTML = peliculas;
-
+    data.results.forEach(pelicula => {                          //  forEach pour parcourir l'objet
+    //console.log(pelicula.title)
     
+    // Pour traiter les images rendez-vous sur ( https://developers.themoviedb.org/3/movies/get-popular-movies ) => GETTING STARTED => images
+      peliculas += `
+        <div class="film">
+          <img  class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"/>    
+          <h3 class = " title " >${pelicula.title}</h3>
+        </div>
+      `
+    });
+    document.querySelector(".container").innerHTML = peliculas;   
   })
 
-  // para obtener solo la pelicula con el ide 550
-  //films.open('GET', 'https://api.themoviedb.org/3/movie/550?api_key=3b8c00b8ac683276327acf4d9aea5b34')
+  // étape 3
   
-  // Para obtener las peliculas mas populares cambiamos 550 por popular ( https://developers.themoviedb.org/3/movies/get-popular-movies )
+  // Pour obtenir uniquement le film avec l'identifiant 550
+  //films.open('GET', 'https://api.themoviedb.org/3/movie/550?api_key=3b8c00b8ac683276327acf4d9aea5b34')
+  //Pour obtenir les films les plus populaires, nous changeons 550 pour populaire ( https://developers.themoviedb.org/3/movies/get-popular-movies )
   films.open('GET', `https://api.themoviedb.org/3/movie/popular?api_key=3b8c00b8ac683276327acf4d9aea5b34&page=${pages}`)
   
+  // étape 4
   films.send();
-  
-
 }
-chargerFilms () 
+
+chargerFilms ()             // Charger la fonction
 
 
